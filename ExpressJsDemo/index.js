c50c8696be3272d6492100d7e2e7bb8858945111
Disha Sahu <sahu.disha21@gmail.com>
2025-10-05 17:30:00 +0530

    Created two seperate projects for understanding basic and project architecture

--- ExpressJsDemo/index.js
+++ ExpressJsDemo/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+// To Serve static page using app.use. Here public folder will be served with default index.html
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// Creating Middleware and calling it from app.use
+// const myMiddleWare = (req, res, next) => {
+//     console.log('Arunesh Middleware')
+//     next();
+// }
+
+// app.use(myMiddleWare);
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -14,7 +25,7 @@ app.get('/about', (req, res) => {
 app.get('/file', (req, res) => {
     // res.sendFile(path.join(__dirname, 'index.html'))
     // res.status(500) //This will send the status code 500
-    res.json({"Arunesh": 32})
+    res.json({ "Arunesh": 32 })
 })
 
 app.listen(port, () => {
